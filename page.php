4567ad45451d216294f9722c907dbb7118363b61
hildeor <hilde.reindal@gmail.com>
2016-11-07 07:41:58 +0100

    Reduced bootstrap cols on pages, styling fix.

--- page.php
+++ page.php
@@ -9,7 +9,7 @@
             <div class="container">
                 <div class="row">
                     <!-- <div class="col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10"> -->
-                      <div class="col-lg-12 col-md-12">
+                      <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
                         <div class="beer-title">
                           <h1 class="article__heading">
                             <!-- <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a> -->
